b33b71128e38222b937a8116483ec76f8623601e
Dane <soredt1@student.op.ac.nz>
2023-05-28 12:28:51 +1200

    players can swap cards out, working on a check to see if the new cards exist in anyones hand or in the discard rack

--- src/Game/GameInstance.js
+++ src/Game/GameInstance.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { doc, getDoc, collection, query, where, onSnapshot, setDoc, writeBatch } from 'firebase/firestore';
+import { doc, getDoc, collection, query, where, onSnapshot, setDoc, writeBatch, updateDoc } from 'firebase/firestore';
 import { db, auth } from '../firebase';
 import deck from './Assets/deck.json';
 
@@ -14,6 +14,7 @@ const GameInstance = () => {
   const [dealtCards, setDealtCards] = useState([]);
   const currentPlayer = players.find((player) => player.id === playerId);
   const [isGameCreator, setIsGameCreator] = useState(false);
+  const [selectedCards, setSelectedCards] = useState([]);
 
   useEffect(() => {
     const fetchGameCreator = async () => {
@@ -32,7 +33,7 @@ const GameInstance = () => {
   useEffect(() => {
     const handsCollectionRef = collection(db, 'games', gameId, 'hands');
     const unsubscribe = onSnapshot(
-      query(handsCollectionRef, where('playerId', 'in', players.map((player) => player.id))),
+      query(handsCollectionRef, where('playerId', '==', playerId)),
       (snapshot) => {
         const newDealtCards = [];
         snapshot.forEach((doc) => {
@@ -44,25 +45,25 @@ const GameInstance = () => {
     );
 
     return () => unsubscribe();
-  }, [gameId, players]);
+  }, [gameId, playerId]);
 
   const dealCards = async () => {
     const shuffledDeck = [...deck].sort(() => Math.random() - 0.5);
-  
+
     const gameRef = doc(db, 'games', gameId);
-  
+
     // Create the "hands" subcollection
     const handsCollectionRef = collection(gameRef, 'hands');
     await setDoc(doc(handsCollectionRef), {}); // Create an empty document in the "hands" subcollection
-  
+
     const batch = writeBatch(db); // Create a batch instance using writeBatch()
-  
+
     players.forEach((player) => {
       const cards = shuffledDeck.splice(0, 5);
       const handRef = doc(handsCollectionRef, player.id); // Use the player ID as the document ID in the "hands" subcollection
       batch.set(handRef, { playerId: player.id, cards });
     });
-  
+
     await batch.commit();
   };
 
@@ -70,37 +71,84 @@ const GameInstance = () => {
     dealCards();
   };
 
-  // Check if players array is null or undefined
-  if (!players) {
-    return <div>Loading...</div>;
-  }
+  const handleCardClick = (cardId) => {
+    if (selectedCards.includes(cardId)) {
+      setSelectedCards(selectedCards.filter((id) => id !== cardId));
+    } else {
+      setSelectedCards([...selectedCards, cardId]);
+    }
+  };
+
+  const handleDeleteClick = async () => {
+    const handRef = doc(db, 'games', gameId, 'hands', currentPlayer.id);
+    const handSnapshot = await getDoc(handRef);
+    const handData = handSnapshot.data();
+    const remainingCards = handData.cards.filter((card) => !selectedCards.includes(card.id));
+  
+    await updateDoc(handRef, { cards: remainingCards });
+  
+    const discardedCardsRef = collection(db, 'games', gameId, 'discardedCards');
+  
+    const batch = writeBatch(db);
+  
+    selectedCards.forEach((cardId) => {
+      const discardedCardRef = doc(discardedCardsRef);
+      batch.set(discardedCardRef, { playerId: currentPlayer.id, cardId });
+    });
+  
+    await batch.commit();
+  
+    setSelectedCards([]);
+  
+    // Check the number of remaining cards
+    if (remainingCards.length < 5) {
+      const shuffledDeck = [...deck].sort(() => Math.random() - 0.5);
+      const newCards = shuffledDeck.splice(0, 5 - remainingCards.length);
+  
+      // Update the hand document with new cards
+      await updateDoc(handRef, {
+        cards: [...remainingCards, ...newCards]
+      });
+    }
+  };  
 
   return (
     <div>
-      <h2>Game Instance:</h2>
-      <p>{gameId}</p>
-      <h3>Players:</h3>
-      {players.map((player) => (
-        <div key={player.id}>
-          <p>{player.name}</p>
-        </div>
-      ))}
-      <h3>Your Cards:</h3>
-      {dealtCards.length > 0 && (
-        <div>
-          {dealtCards.find((hand) => hand.player === currentPlayer.id).cards.map((card, cardIndex) => (
-            <span key={cardIndex}>
-              {card.name} of {card.suit}
-              {cardIndex !== dealtCards.find((hand) => hand.player === currentPlayer.id).cards.length - 1 && ', '}
-            </span>
-          ))}
-        </div>
-      )}
+      <h1>Game Instance</h1>
+      <h2>Game ID: {gameId}</h2>
+      <h3>Player ID: {playerId}</h3>
+      <h3>Current Player: {currentPlayer.name}</h3>
       {isGameCreator && (
-        <button onClick={handleDealClick}>Deal</button>
+        <div>
+          <button onClick={handleDealClick}>Deal Cards</button>
+        </div>
       )}
+      <h4>Your Cards:</h4>
+      {dealtCards.map((dealt) => {
+        if (dealt.player === playerId) {
+          return (
+            <div key={dealt.player}>
+              <ul>
+                {dealt.cards.map((card) => (
+                  <li
+                    key={card.id}
+                    style={{ backgroundColor: selectedCards.includes(card.id) ? 'yellow' : 'white' }}
+                    onClick={() => handleCardClick(card.id)}
+                  >
+                    {`${card.name} of ${card.suit}`}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          );
+        }
+        return null;
+      })}
+      <div>
+        <button onClick={handleDeleteClick}>Delete Selected Cards</button>
+      </div>
     </div>
-  );
+  );  
 };
 
 export default GameInstance;
