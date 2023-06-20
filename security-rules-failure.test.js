e17b7390fb9b4b8f2147142cf7f2dad2be7fef6a
Dane <soredt1@student.op.ac.nz>
2023-06-20 22:20:24 +1200

    created some tests for checking authorized users can create games docs and non authorized users cannont create games docs

---
+++ security-rules-failure.test.js
@@ -0,0 +1,55 @@
+const { initializeTestEnvironment, assertFails } = require('@firebase/rules-unit-testing');
+const { firestore } = require('firebase-admin');
+const firebase = require('firebase/app');
+
+// Mock the necessary Firestore methods
+jest.mock('firebase/app', () => {
+  return {
+    initializeApp: jest.fn(),
+    firestore: jest.fn(() => ({
+      collection: jest.fn(() => ({
+        doc: jest.fn(() => ({
+          get: jest.fn(() => Promise.resolve({})),
+          set: jest.fn(() => Promise.reject(new Error('Unauthorized'))),
+          update: jest.fn(() => Promise.reject(new Error('Unauthorized'))),
+          delete: jest.fn(() => Promise.reject(new Error('Unauthorized'))),
+        })),
+        get: jest.fn(() => Promise.resolve({})),
+      })),
+    })),
+  };
+});
+
+// Import and test your security rules for failure cases
+describe('Firestore security rules (failure cases)', () => {
+  beforeAll(async () => {
+    // Initialize the Firebase app
+    firebase.initializeApp({
+      projectId: 'dane-poker', // Replace with your project ID
+    });
+    // Initialize the Firestore emulator
+    await initializeTestEnvironment({ projectId: 'dane-poker', firestore });
+  });
+
+  it('should deny create access to a game document if not authorized', async () => {
+    const db = firebase.firestore();
+    const gameDocRef = db.collection('games').doc('test-game-id');
+
+    await assertFails(gameDocRef.set({ data: 'test' }));
+  });
+
+  it('should deny update access to a game document if not authorized', async () => {
+    const db = firebase.firestore();
+    const gameDocRef = db.collection('games').doc('test-game-id');
+
+    await assertFails(gameDocRef.update({ data: 'test' }));
+  });
+
+  it('should deny delete access to a game document if not authorized', async () => {
+    const db = firebase.firestore();
+    const gameDocRef = db.collection('games').doc('test-game-id');
+
+    await assertFails(gameDocRef.delete());
+  });
+
+});
---
+++ security-rules.test.js
@@ -0,0 +1,72 @@
+const { initializeTestEnvironment, assertFails, assertSucceeds } = require('@firebase/rules-unit-testing');
+const { firestore } = require('firebase-admin');
+const firebase = require('firebase/app');
+
+// Mock the necessary Firestore methods
+jest.mock('firebase/app', () => {
+  return {
+    initializeApp: jest.fn(),
+    firestore: jest.fn(() => ({
+      collection: jest.fn(() => ({
+        doc: jest.fn(() => ({
+          get: jest.fn(() => Promise.resolve({})),
+          set: jest.fn(() => Promise.resolve()),
+          update: jest.fn(),
+          delete: jest.fn(),
+        })),
+        get: jest.fn(() => Promise.resolve({})),
+      })),
+    })),
+  };
+});
+
+// Import and test your security rules
+describe('Firestore security rules', () => {
+  beforeAll(async () => {
+    // Initialize the Firebase app
+    firebase.initializeApp({
+      projectId: 'dane-poker', // Replace with your project ID
+    });
+    // Initialize the Firestore emulator
+    await initializeTestEnvironment({ projectId: 'dane-poker', firestore });
+  });
+  
+  it('should allow read access to a user document if authorized', async () => {
+    const db = firebase.firestore();
+    const userDocRef = db.collection('users').doc('test-user-id');
+    // Attempt to read the user document
+    await assertSucceeds(userDocRef.get());
+  });
+
+  it('should allow read access to the user collection', async () => {
+    const db = firebase.firestore();
+    const usersCollectionRef = db.collection('users');
+
+    // Attempt to read the user collection
+    await assertSucceeds(usersCollectionRef.get());
+  });
+
+  it('should allow create access to a game document if authorized', async () => {
+    const db = firebase.firestore();
+    const gameDocRef = db.collection('games').doc('test-game-id');
+
+    // Attempt to create the game document
+    await assertSucceeds(gameDocRef.set({ data: 'test' }));
+  });
+
+  it('should allow update access to a game document if authorized', async () => {
+    const db = firebase.firestore();
+    const gameDocRef = db.collection('games').doc('test-game-id');
+
+    // Attempt to update the game document
+    await assertSucceeds(gameDocRef.update({ data: 'test' }));
+  });
+
+  it('should allow delete access to a game document if authorized', async () => {
+    const db = firebase.firestore();
+    const gameDocRef = db.collection('games').doc('test-game-id');
+
+    // Attempt to delete the game document
+    await assertSucceeds(gameDocRef.delete());
+  });
+});
